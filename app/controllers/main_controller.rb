a552e32e584f3ef47f6bf791ae1f1d984f198b4d
Troy Anderson <the.inventor@gmail.com>
2012-07-21 16:20:23 -0700

    initial commit - dont hack my arduino

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,37 @@
+class MainController < ApplicationController
+  require 'open-uri'
+
+  require 'json'
+  TELEDUINO_KEY = "9EB1A6461DE6C8F5BC2999428B9E3D25"
+
+
+  def index
+    base_url = "http://us01.proxy.teleduino.org/api/1.0/328.php?k=#{TELEDUINO_KEY}"
+    if params[:three_output]
+      query = "&r=definePinMode&pin=3&mode=1"
+    elsif params[:three] == "on"
+      query = "&r=setDigitalOutput&pin=3&output=1"
+    elsif params[:three] == "off"
+      query = "&r=setDigitalOutput&pin=3&output=0"
+    elsif params[:five_output]
+      query = "&r=definePinMode&pin=5&mode=1"
+    elsif params[:five] == "on"
+      query = "&r=setDigitalOutput&pin=5&output=1"
+    elsif params[:five] == "off"
+      query = "&r=setDigitalOutput&pin=5&output=0"
+    elsif params[:six_output]
+      query = "&r=definePinMode&pin=6&mode=1"
+    elsif params[:six] == "on"
+      query = "&r=setDigitalOutput&pin=6&output=1"
+    elsif params[:six] == "off"
+      query = "&r=setDigitalOutput&pin=6&output=0"
+    else
+      query = "&r=getUptime"
+    end
+
+    @command = base_url + query
+    buffer = open(@command, "UserAgent" => "Ruby-Wget").read
+    @result = JSON.parse(buffer)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TroyDuino::Application.config.secret_token = 'f5fdfb62845f630bc3cb1106679eeb6040db94c64a1dc5676e6d4f1cfe79c4abf01f766522e35c7a077f184886b1f30f48fbde39c9339f0701cc5ea67a732e60'
